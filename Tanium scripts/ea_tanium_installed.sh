c9aa12fb85c13c14232fff70c7709b1ea9088e5b
rqomsiya <qomsiya@gmail.com>
2017-03-21 20:58:54 -0700

    Update ea_tanium_installed.sh

--- Tanium scripts/ea_tanium_installed.sh
+++ Tanium scripts/ea_tanium_installed.sh
@@ -1,8 +1,25 @@
-#!/bin/bash
-if [[ -f "/Library/LaunchDaemons/com.tanium.taniumclient.plist" ]]; then 
-	isTaniumInstalled="Installed"
+#!/bin/sh
+
+# Script to check if the Tanium LaunchDaemon is installed and loaded
+# If Tanium Launch Daemon is installed and loaded the result will be "Installed and loaded".
+# If Tanium file is not present the result will be "Not Installed".
+# If Tanium file is present but the LaunchDaemon is not loaded the result will be "Not loaded".
+
+
+
+TaniumCheckLaunchDaemon="/Library/LaunchDaemons/com.tanium.taniumclient.plist"
+
+if [[ -f "$TaniumCheckLaunchDaemon" ]]; then
+	TaniumCheckLoaded=$(/bin/launchctl list | grep com.tanium.taniumclient)
+		
+		#if string is not empty
+		if [ -n "${TaniumCheckLoaded}" ]; then
+		result="Installed and loaded"
+	else
+	result="Not loaded"
+	fi
 else
-	isTaniumInstalled="Not Installed"
+	result="Not Installed"
 fi
 
-echo "<result>$isTaniumInstalled</result>"
+echo "<result>$result</result>"
