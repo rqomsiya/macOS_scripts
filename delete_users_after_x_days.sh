7809a44455b16b0bef9770b51bc65f3762c1ef96
rqomsiya <qomsiya@gmail.com>
2017-04-06 16:03:09 -0700

    Create delete_users_after_x_days.sh

---
+++ delete_users_after_x_days.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+userList=`dscl . list /Users UniqueID | awk '$2 > 1000 {print $1}'`
+echo "Deleting account and home directory for the following users..."
+for a in $userList ; do
+find /Users -type d -maxdepth 1 -mindepth 1 -not -name "*.*" -mtime +90 | grep "$a";
+if [[ $? == 0 ]]; then
+dscl . delete /Users/"$a"; #delete the account
+rm -r /Users/"$a"; #delete the home directory
+fi
+done
