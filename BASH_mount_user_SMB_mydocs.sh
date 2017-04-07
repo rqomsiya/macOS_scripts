8a1fd3dcf208dd442374da6e62bfffb9af79b3af
rqomsiya <qomsiya@gmail.com>
2017-04-06 19:51:55 -0700

    Update BASH_mount_user_SMB_mydocs.sh

--- BASH_mount_user_SMB_mydocs.sh
+++ BASH_mount_user_SMB_mydocs.sh
@@ -4,13 +4,14 @@ theuser=$(/usr/bin/who | awk '/console/{ print $1 }')
 /usr/bin/osascript > /dev/null << EOT
 
 	tell application "Finder" 
-	activate
-	mount volume "smb://capgroup.com/mydocs/${theuser}/my documents/"
+		if not (disk "my documents" exists) then
+			mount volume "smb://capgroup.com/mydocs/${theuser}/my documents/"
+		end if
 	end tell
 
 
 EOT
 
-echo $theuser
+echo $theuser My Documents server volume mounted on desktop.
 killall cfprefsd
 defaults write com.apple.finder ShowMountedServersOnDesktop true
