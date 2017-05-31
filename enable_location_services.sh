07a0e15e876bc6edd94acfc2d09e58fb29107c51
rqomsiya <qomsiya@gmail.com>
2017-05-31 14:18:39 -0700

    Create enable_location_services.sh

---
+++ enable_location_services.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Unload locationd
+launchctl unload /System/Library/LaunchDaemons/com.apple.locationd.plist
+
+## Write enabled value to locationd plist
+defaults write /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd LocationServicesEnabled -int 1
+
+## Fix Permissions for the locationd folder
+chown -R _locationd:_locationd /var/db/locationd
+
+## Reload locationd
+launchctl load /System/Library/LaunchDaemons/com.apple.locationd.plist
+
+exit 0
