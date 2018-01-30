d593b869ffdaa18491f8c7b9368f5b6f65a794ca
rqomsiya <qomsiya@gmail.com>
2018-01-30 10:05:23 -0800

    Update macOS_Upgrade_10.10.5 to 10.12.6.sh

--- macOS Sierra Self Service Upgrade/macOS_Upgrade_10.10.5 to 10.12.6.sh
+++ macOS Sierra Self Service Upgrade/macOS_Upgrade_10.10.5 to 10.12.6.sh
@@ -1,8 +1,56 @@
 #!/bin/bash
 
-# Script includes IAQuitInsteadOfReboot key (forces installer to quit instead of proceed with a reboot).
-# Reboot will be handled by JAMF policy (authenticated reboot).
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Copyright (c) 2018 Jamf.  All rights reserved.
+#
+#       Redistribution and use in source and binary forms, with or without
+#       modification, are permitted provided that the following conditions are met:
+#               * Redistributions of source code must retain the above copyright
+#                 notice, this list of conditions and the following disclaimer.
+#               * Redistributions in binary form must reproduce the above copyright
+#                 notice, this list of conditions and the following disclaimer in the
+#                 documentation and/or other materials provided with the distribution.
+#               * Neither the name of the Jamf nor the names of its contributors may be
+#                 used to endorse or promote products derived from this software without
+#                 specific prior written permission.
+#
+#       THIS SOFTWARE IS PROVIDED BY JAMF SOFTWARE, LLC "AS IS" AND ANY
+#       EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#       WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#       DISCLAIMED. IN NO EVENT SHALL JAMF SOFTWARE, LLC BE LIABLE FOR ANY
+#       DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#       (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#       LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#       ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#       SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# This script was designed to be used in a Self Service policy to ensure specific
+# requirements have been met before proceeding with an inplace upgrade of the macOS,
+# as well as to address changes Apple has made to the ability to complete macOS upgrades
+# silently.
+#
+# REQUIREMENTS:
+#           - Jamf Pro
+#           - Latest Version of the macOS Installer (must be 10.12.4 or later)
+#           - Look over the USER VARIABLES and configure as needed.
+#
+#
+# For more information, visit https://github.com/kc9wwh/macOSUpgrade
+#
+#
+# Written by: Joshua Roskos | Professional Services Engineer | Jamf
+#
+# Created On: January 5th, 2017
+# Updated On: January 30th, 2018
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # USER VARIABLES
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -50,58 +98,61 @@ icon="$OSInstaller/Contents/Resources/InstallAssistant.icns"
 # SYSTEM CHECKS
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+##Get Current User
+currentUser=$( stat -f %Su /dev/console )
+
 ##Check if device is on battery or ac power
 pwrAdapter=$( /usr/bin/pmset -g ps )
 if [[ ${pwrAdapter} == *"AC Power"* ]]; then
-		pwrStatus="OK"
-		/bin/echo "Power Check: OK - AC Power Detected"
+    pwrStatus="OK"
+    /bin/echo "Power Check: OK - AC Power Detected"
 else
-		pwrStatus="ERROR"
-		/bin/echo "Power Check: ERROR - No AC Power Detected"
+    pwrStatus="ERROR"
+    /bin/echo "Power Check: ERROR - No AC Power Detected"
 fi
 
 ##Check if free space > 15GB
 osMinor=$( /usr/bin/sw_vers -productVersion | awk -F. {'print $2'} )
 if [[ $osMinor -ge 12 ]]; then
-		freeSpace=$( /usr/sbin/diskutil info / | grep "Available Space" | awk '{print $6}' | cut -c 2- )
+    freeSpace=$( /usr/sbin/diskutil info / | grep "Available Space" | awk '{print $6}' | cut -c 2- )
 else
-		freeSpace=$( /usr/sbin/diskutil info / | grep "Free Space" | awk '{print $6}' | cut -c 2- )
+    freeSpace=$( /usr/sbin/diskutil info / | grep "Free Space" | awk '{print $6}' | cut -c 2- )
 fi
 
 if [[ ${freeSpace%.*} -ge 15000000000 ]]; then
-		spaceStatus="OK"
-		/bin/echo "Disk Check: OK - ${freeSpace%.*} Bytes Free Space Detected"
+    spaceStatus="OK"
+    /bin/echo "Disk Check: OK - ${freeSpace%.*} Bytes Free Space Detected"
 else
-		spaceStatus="ERROR"
-		/bin/echo "Disk Check: ERROR - ${freeSpace%.*} Bytes Free Space Detected"
+    spaceStatus="ERROR"
+    /bin/echo "Disk Check: ERROR - ${freeSpace%.*} Bytes Free Space Detected"
 fi
 
 ##Check for existing OS installer
 if [ -e "$OSInstaller" ]; then
-	/bin/echo "$OSInstaller found, checking version."
-	OSVersion=`/usr/libexec/PlistBuddy -c 'Print :"System Image Info":version' "$OSInstaller/Contents/SharedSupport/InstallInfo.plist"`
-	/bin/echo "OSVersion is $OSVersion"
-	if [ $OSVersion = $version ]; then
-		downloadOS="No"
-	else
-		downloadOS="Yes"
-		##Delete old version.
-		/bin/echo "Installer found, but old. Deleting..."
-		/bin/rm -rf "$OSInstaller"
-	fi
+  /bin/echo "$OSInstaller found, checking version."
+  OSVersion=`/usr/libexec/PlistBuddy -c 'Print :"System Image Info":version' "$OSInstaller/Contents/SharedSupport/InstallInfo.plist"`
+  /bin/echo "OSVersion is $OSVersion"
+  if [ $OSVersion = $version ]; then
+    downloadOS="No"
+  else
+    downloadOS="Yes"
+    ##Delete old version.
+    /bin/echo "Installer found, but old. Deleting..."
+    /bin/rm -rf "$OSInstaller"
+  fi
 else
-	downloadOS="Yes"
+  downloadOS="Yes"
 fi
 
 ##Download OS installer if needed
 if [ $downloadOS = "Yes" ]; then
-	/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper \
-			-windowType utility -title "$title"  -alignHeading center -alignDescription left -description "$dldescription" \
-			-button1 Ok -defaultButton 1 -icon "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/SidebarDownloadsFolder.icns" -iconSize 100
-	##Run policy to cache installer
-	/usr/local/jamf/bin/jamf policy -event $download_trigger
+  /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper \
+      -windowType utility -title "$title"  -alignHeading center -alignDescription left -description "$dldescription" \
+      -button1 Ok -defaultButton 1 -icon "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/SidebarDownloadsFolder.icns" -iconSize 100
+  ##Run policy to cache installer
+  /usr/local/jamf/bin/jamf policy -event $download_trigger
 else
-	/bin/echo "$macOSname installer with $version was already present, continuing..."
+  /bin/echo "$macOSname installer with $version was already present, continuing..."
 fi
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -115,8 +166,6 @@ fi
 ## Clean up files
 /bin/rm -fdr "$OSInstaller"
 /bin/sleep 2
-#Cleanup IAQuitInsteadOfReboot key
-/usr/bin/defaults delete /Library/Preferences/.GlobalPreferences IAQuitInsteadOfReboot
 ## Update Device Inventory
 /usr/local/jamf/bin/jamf recon
 ## Remove LaunchDaemon
@@ -137,16 +186,16 @@ cat << EOF > /Library/LaunchDaemons/com.jamfps.cleanupOSInstall.plist
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-		<key>Label</key>
-		<string>com.jamfps.cleanupOSInstall</string>
-		<key>ProgramArguments</key>
-		<array>
-				<string>/bin/bash</string>
-				<string>-c</string>
-				<string>/usr/local/jamfps/finishOSInstall.sh</string>
-		</array>
-		<key>RunAtLoad</key>
-		<true/>
+    <key>Label</key>
+    <string>com.jamfps.cleanupOSInstall</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/bin/bash</string>
+        <string>-c</string>
+        <string>/usr/local/jamfps/finishOSInstall.sh</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
 </dict>
 </plist>
 EOF
@@ -154,6 +203,40 @@ EOF
 ##Set the permission on the file just made.
 /usr/sbin/chown root:wheel /Library/LaunchDaemons/com.jamfps.cleanupOSInstall.plist
 /bin/chmod 644 /Library/LaunchDaemons/com.jamfps.cleanupOSInstall.plist
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# LAUNCH AGENT FOR FILEVAUTL AUTHENTICATED REBOOTS
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+cat << EOP > /Library/LaunchAgents/com.apple.install.osinstallersetupd.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>com.apple.install.osinstallersetupd</string>
+    <key>LimitLoadToSessionType</key>
+    <string>Aqua</string>
+    <key>MachServices</key>
+    <dict>
+        <key>com.apple.install.osinstallersetupd</key>
+        <true/>
+    </dict>
+    <key>TimeOut</key>
+    <integer>Aqua</integer>
+    <key>OnDemand</key>
+    <true/>
+    <key>ProgramArguments</key>
+    <array>
+        <string>$OSInstaller/Contents/Frameworks/OSInstallerSetup.framework/Resources/osinstallersetupplaind</string>
+    </array>
+</dict>
+</plist>
+EOP
+
+##Set the permission on the file just made.
+/usr/sbin/chown root:wheel /Library/LaunchAgents/com.apple.install.osinstallersetupd.plist
+/bin/chmod 644 /Library/LaunchAgents/com.apple.install.osinstallersetupd.plist
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # APPLICATION
@@ -164,33 +247,36 @@ EOF
 caffeinatePID=$(echo $!)
 
 if [[ ${pwrStatus} == "OK" ]] && [[ ${spaceStatus} == "OK" ]]; then
-		##Launch jamfHelper
-		if [[ ${userDialog} == 0 ]]; then
-				/bin/echo "Launching jamfHelper as FullScreen..."
-				/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType fs -title "" -icon "$icon" -heading "$heading" -description "$description" &
-				jamfHelperPID=$(echo $!)
-		fi
-		if [[ ${userDialog} == 1 ]]; then
-				/bin/echo "Launching jamfHelper as Utility Window..."
-				/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -title "$title" -icon "$icon" -heading "$heading" -description "$description" -iconSize 100 &
-				jamfHelperPID=$(echo $!)
-		fi
-		
-		#Set key to Quit binary installer instead of reboot. Reboot will be handled by JAMF policy. This key is deleted as part of first boot script.
-		/usr/bin/defaults write /Library/Preferences/.GlobalPreferences IAQuitInsteadOfReboot -bool YES
-		##Begin Upgrade
-		/bin/echo "Launching startosinstall..."
-		"$OSInstaller/Contents/Resources/startosinstall" --applicationpath "$OSInstaller" --nointeraction --pidtosignal $jamfHelperPID &
-		/bin/sleep 3
+    ##Launch jamfHelper
+    if [[ ${userDialog} == 0 ]]; then
+        /bin/echo "Launching jamfHelper as FullScreen..."
+        /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType fs -title "" -icon "$icon" -heading "$heading" -description "$description" &
+        jamfHelperPID=$(echo $!)
+    fi
+    if [[ ${userDialog} == 1 ]]; then
+        /bin/echo "Launching jamfHelper as Utility Window..."
+        /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -title "$title" -icon "$icon" -heading "$heading" -description "$description" -iconSize 100 &
+        jamfHelperPID=$(echo $!)
+    fi
+    ##Load LaunchAgent
+    if [[ ${currentUser} != "root" ]]; then
+        userID=$( id -u ${currentUser} )
+        launchctl bootstrap gui/${userID} /Library/LaunchAgents/com.apple.install.osinstallersetupd.plist
+    fi
+    ##Begin Upgrade
+    /bin/echo "Launching startosinstall..."
+    "$OSInstaller/Contents/Resources/startosinstall" --applicationpath "$OSInstaller" --nointeraction --pidtosignal $jamfHelperPID &
+    /bin/sleep 3
 else
-		## Remove Script
-		/bin/rm -f /usr/local/jamfps/finishOSInstall.sh
-		/bin/rm -f /Library/LaunchDaemons/com.jamfps.cleanupOSInstall.plist
+    ## Remove Script
+    /bin/rm -f /usr/local/jamfps/finishOSInstall.sh
+    /bin/rm -f /Library/LaunchDaemons/com.jamfps.cleanupOSInstall.plist
+    /bin/rm -f /Library/LaunchAgents/com.apple.install.osinstallersetupd.plist
 
-		/bin/echo "Launching jamfHelper Dialog (Requirements Not Met)..."
-		/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -title "$title" -icon "$icon" -heading "Requirements Not Met" -description "We were unable to prepare your computer for $macOSname. Please ensure you are connected to power and that you have at least 15GB of Free Space.
+    /bin/echo "Launching jamfHelper Dialog (Requirements Not Met)..."
+    /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -title "$title" -icon "$icon" -heading "Requirements Not Met" -description "We were unable to prepare your computer for $macOSname. Please ensure you are connected to power and that you have at least 15GB of Free Space.
 
-		If you continue to experience this issue, please contact the TSD x.55777." -iconSize 100 -button1 "OK" -defaultButton 1
+    If you continue to experience this issue, please contact the TSD x.55777." -iconSize 100 -button1 "OK" -defaultButton 1
 
 fi
 
