b6af900ffe75cb98485f81a7e39d6874e8cfef12
rqomsiya <qomsiya@gmail.com>
2018-02-09 16:25:26 -0800

    Update security_udpate_notification.sh

--- security_udpate_notification.sh
+++ security_udpate_notification.sh
@@ -5,7 +5,7 @@ loggedInUser=$( ls -l /dev/console | awk '{print $3}' )
 # Get the PID of the logged in user
 loggedInPID=$( ps -axj | awk "/^$loggedInUser/ && /Dock.app/ {print \$2;exit}" )
 
-install=$(/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -title "CG IT Security Updates" -heading "Security updates pending installation" -alignHeading center -description "Please SAVE all work and quit from all applications. You will be logged off, updates will be installed and your Mac will restart." -alignDescription center -icon /private/tmp/capitalgroup_logo.png -button1 "Install" -button2 "Cancel" -defaultButton 1 -cancelButton 2)
+install=$(/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -title "CG IT Security Updates" -heading "Security updates pending installation" -alignHeading center -description "Please SAVE all work and quit from all applications. You will be logged off, updates will be installed and your Mac will restart." -alignDescription center -icon /private/tmp/cg_logo.png -button1 "Install" -button2 "Cancel" -defaultButton 1 -cancelButton 2)
 
 # If the user clicks install
 if [ "$install" == "0" ]; then
