d7587e636d215b881e4590445e85693867019f6c
rqomsiya <qomsiya@gmail.com>
2018-03-02 21:54:20 -0800

    Update delete_all_computer_level_profiles.sh

--- delete_all_computer_level_profiles.sh
+++ delete_all_computer_level_profiles.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Get a list of all computer level profiles installed on the computer and remove every one of them. Note lower case -r and not -R (which is for user level profiles)
+# Get a list from all profiles installed on the computer and remove every one of them
 
 for identifier in $(profiles -C | awk "/attribute/" | awk '{print $4}')
 do profiles -r -p $identifier
+done
+
+sleep 5
+
+for identifier in $(profiles -C | awk "/attribute/" | awk '{print $4}')
+do profiles -R -p $identifier
 done
