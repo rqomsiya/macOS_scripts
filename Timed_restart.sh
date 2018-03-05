0a42b9892dbac39fc90d132c69239b6a2575f80e
rqomsiya <qomsiya@gmail.com>
2018-03-04 16:54:13 -0800

    Update Timed_restart.sh

--- Timed_restart.sh
+++ Timed_restart.sh
@@ -9,7 +9,7 @@ cdTitle="CG IT: Restart required for Security Updates"
 rm -f /tmp/hpipe
 mkfifo /tmp/hpipe
 sleep 0.2
-$cdPath progressbar --title "$cdTitle" --text "Please SAVE all work. Your Mac will restart in 4 hours..." \
+$cdPath progressbar --title "$cdTitle" --text "Please SAVE all work and restart now. Your Mac will restart in 4 hours..." \
 --posX "left" --posY "top" --width 300 --float \
 --icon-file "/tmp/cg_icon.png" \
 --icon-height 48 --icon-width 48 --height 90 < /tmp/hpipe &
