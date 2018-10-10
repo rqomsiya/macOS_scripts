b46af42b0aa82c68c47185306dd90635aa417c0a
rqomsiya <qomsiya@gmail.com>
2018-10-10 11:56:16 -0700

    Create Uninstall_Crashplan.sh

---
+++ Uninstall_Crashplan.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script to uninstall CrashPlan installations that are NOT per user
+sudo /Library/Application\ Support/CrashPlan/Uninstall.app/Contents/Resources/uninstall.sh 
+
+exit 0
