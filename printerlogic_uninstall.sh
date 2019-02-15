9fbe55725703c52a174d2f3ca23c6060bb493d01
rqomsiya <qomsiya@gmail.com>
2019-02-15 10:07:24 -0800

    Update printerlogic_uninstall.sh

--- printerlogic_uninstall.sh
+++ printerlogic_uninstall.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
+FILE=/opt/PrinterInstallerClient/bin/uninstall.sh 
 
-echo "Stopping application"
-osascript -e 'tell application "PrinterInstallerClient" to quit' || true
-
-echo "Removing services"
-launchctl unload /Library/LaunchDaemons/com.printerlogic.client.plist || true
-launchctl remove com.printerlogic.client || true
-launchctl unload /Library/LaunchAgents/com.printerlogic.client.scheduled_refresh.plist || true
-# Remove any scheduled_refresh tasks from older versions
-launchctl remove com.printerlogic.client.scheduled_refresh || true
-launchctl unload /Library/LaunchAgents/com.printerlogic.client.scheduled_refresh.*.plist || true
-killall -9 PrinterInstallerClientService || true
-killall -9 PrinterInstallerClientInterface || true
-rm -f /Library/LaunchDaemons/com.printerlogic.client* || true
-rm -f /Library/LaunchAgents/com.printerlogic.client* || true
-
-echo "Unregistering apps"
-pkgutil --packages | grep printerlogic | xargs -n 1 pkgutil --forget 2> /dev/null || true
-rm -rf /Applications/PrinterInstallerClient.app || true
-
-echo "Removing web browser plugin"
-rm -f /Library/Internet\ Plug-Ins/npPrinterInstallerClientPlugin.plugin || true
-
-echo "Deleting files"
-rm -rf /opt/PrinterInstallerClient/tmp || true
-rm -f /Applications/Printer\ Installer\ Client.app || true
-rm -rf /opt/PrinterInstallerClient || true
-
-echo "Uninstallation complete"
+if [ -f $FILE ]; then
+	echo "The file '$FILE' exists. Uninstall in progress"
+	/opt/PrinterInstallerClient/bin/uninstall.sh 
+else
+	echo "The file '$FILE' in not found."
+fi
+exit 0
