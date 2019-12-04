df34f747ff1ce9cbf8db67ebce3897e79132e89e
rqomsiya <qomsiya@gmail.com>
2019-12-04 10:47:26 -0800

    Create mindmanager_license.sh

---
+++ mindmanager_license.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#####################################################################################################
+#
+# ABOUT THIS PROGRAM
+#
+# NAME
+#   MindManagerSettings.sh -- Set License and accept EULA
+#
+#
+####################################################################################################
+#
+# HISTORY
+#
+#   Version: 1.0
+#   - Philipp Reinheimer, 14.04.18
+#
+####################################################################################################
+
+
+#---Variables---#
+currentuser=$(/bin/ls -la /dev/console | /usr/bin/cut -d ' ' -f 4)
+
+#---LicenseKey and Settings---#
+su -l $currentuser -c "defaults -currentHost write com.mindjet.mindmanager.11 LicenseKey 'ME20-M1M-ME2D-2A21-7E6B'"
+su -l $currentuser -c "defaults -currentHost write com.mindjet.mindmanager.11 ShowEULA -int 0"
+su -l $currentuser -c "defaults -currentHost write com.mindjet.mindmanager.11 Edition -int 0"
+
+su -l $currentuser -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.11 DeviceAutoUpdateDisabled -bool false"
+su -l $currentuser -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.11 FirstLaunch -bool false"
+su -l $currentuser -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.11 StartupDialogPolicy -int 3"
+su -l $currentuser -c "defaults write /Users/$currentuser/Library/Preferences/com.mindjet.mindmanager.11 NewDocumentOnStart -int 1"
+
+echo $currentuser
+
+exit 0
