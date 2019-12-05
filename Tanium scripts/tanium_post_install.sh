9618c7012c52d6d40e1e1d0c850f1423450ce9fb
rqomsiya <qomsiya@gmail.com>
2019-12-05 13:46:52 -0800

    Update tanium_post_install.sh

--- Tanium scripts/tanium_post_install.sh
+++ Tanium scripts/tanium_post_install.sh
@@ -1,1 +1,19 @@
- /Library/Tanium/TaniumClient config set ServerNameList tants1.capgroup.com,148.107.22.53,148.107.28.53
+#!/bin/bash
+
+# Determine working directory
+install_dir=`dirname $0`
+
+#Office Installers
+Tanium="TaniumClient-7.2.314.3608.pkg"
+
+#Install Tanium client
+/usr/sbin/installer -dumplog -verbose -pkg $install_dir/"$Tanium" -target "$3" -allowUntrusted
+
+sleep 10
+
+/Library/Tanium/TaniumClient/TaniumClient config set ServerNameList tants1.capgroup.com,148.107.22.53,148.107.28.53
+
+#Load the launchD
+sudo launchctl load /Library/LaunchDaemons/com.tanium.taniumclient.plist
+
+exit 0
